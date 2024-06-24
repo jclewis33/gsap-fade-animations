64cffc93aad14037ac8938e1c5755093175b31b8
jclewis33 <109756608+jclewis33@users.noreply.github.com>
2024-06-24 13:20:53 -0500

    Initial Commit

--- app.js
+++ app.js
@@ -2,7 +2,91 @@
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  alert("hello world");
+  //alert("hello world");
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  ///////////////////// FADE IN ANIMATION ////////////////
+
+  const fadeInElements = gsap.utils.toArray("[fade-in]");
+
+  fadeInElements.forEach((fadeInElement) => {
+    gsap.fromTo(
+      fadeInElement,
+      { opacity: 0, y: 20 }, // Initial state
+      {
+        // Final state
+        opacity: 1,
+        y: 0,
+        duration: 0.5,
+        scrollTrigger: {
+          trigger: fadeInElement,
+          start: "top 60%", // When top of element reaches 60% in the viewport
+          end: "bottom 50%", // When bottom of element reaches 50% in the viewport
+          markers: "true",
+        },
+      }
+    );
+  });
+
+  ///////////////////// END FADE IN ANIMATION ////////////////
+
+  ///////////////////// BEGIN FADE LIST ANIMATION ////////////////
+
+  const fadeInLists = gsap.utils.toArray("[fade-list]");
+
+  fadeInLists.forEach((fadeInList) => {
+    gsap.fromTo(
+      fadeInList.children,
+      { opacity: 0, y: 20 }, // Initial state
+      {
+        // Final state
+        opacity: 1,
+        y: 0,
+        duration: 0.5,
+        stagger: 0.2,
+        scrollTrigger: {
+          trigger: fadeInList,
+          start: "top 60%", // When top of element reaches 60% in the viewport
+          end: "bottom 50%", // When bottom of element reaches 50% in the viewport
+          markers: "true",
+        },
+      }
+    );
+  });
+
+  ///////////////////// END FADE LIST ANIMATION ////////////////
+
+  //////////// REFRESH SCROLLTRIGGER after everything has loaded //////////////
+  //Using a setTimeout to ensure everything has loaded on the page.
+
+  setTimeout(() => {
+    ScrollTrigger.refresh(true);
+    console.log("Scroll Trigger has been refreshed");
+  }, 1500); //Refreshes ScrollTrigger after 1.5 seconds to ensure everything has completed loaded and is settled into place.
+
+  ///////////// MANUAL SCROLLTRIGGER REFRESH /////////////////
+
+  //Meant to esnure Fade In Animiation above runs properly. Placed selectively to refresh scrollTrigger on sections that were affecting scrollTrigger animations afterwards. These are typically sections that have fade-in animations where images are loading, or sections that have dynamic content that needs to load. If proper aspect-ratio's are applied to all images on the site, you should not need this option.
+
+  //But if you need to use it, place refresh in the attributes panel on the element at which you want Scroll Trigger to be refreshed. No value is needed.
+
+  const refreshElements = gsap.utils.toArray("[refresh]");
+
+  refreshElements.forEach((refreshElement) => {
+    const id = refreshElement.id; // Get the ID of the element
+    const message = `${id} has refreshed` || "section refreshed - no ID added";
+    const refresh = gsap.timeline({
+      scrollTrigger: {
+        trigger: refreshElement,
+        start: "top bottom",
+        onLeave: () => {
+          console.log(message);
+          ScrollTrigger.refresh();
+        },
+      },
+    });
+  });
 });
 
 /*code to add into webflor project
